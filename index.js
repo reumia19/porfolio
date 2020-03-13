ff809d16d189014b6ecc2d76e8533966b6765d61
reumia19 <56555888+reumia19@users.noreply.github.com>
2020-03-13 21:35:32 +0900

    fxxking awesome css

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-alert('아니 아직 아무것도 안 만들었는데... 벌써 올 줄은 몰랐지.. 나중에 오세요');
-const myName =document.querySelector("#myName");
-myName.style.color = "white";
+// alert('아니 아직 아무것도 안 만들었는데... 벌써 올 줄은 몰랐지.. 나중에 오세요');
+const title =document.querySelector("#title");
+const CLICKED_CLASS = "clicked";
+
+function handleClick(){
+	const currentClass=title.className;
+	if (currentClass!==CLICKED_CLASS) {
+		title.className=CLICKED_CLASS;
+	}else{
+		title.className="";
+	}
+}
+
+function init(){
+	title.addEventListener("click",handleClick);
+}
+init();
