92bf7a05daa3185483ef0e36767cd73cc42e613f
reumia19 <56555888+reumia19@users.noreply.github.com>
2020-03-13 23:07:32 +0900

    update clock

--- clock.js
+++ clock.js
@@ -1,2 +1,15 @@
 const clockContainer = document.querySelector(".js-clock")
-	clockTile = document.querySelector("h3");
+	clockTime = document.querySelector("h3");
+
+function getTime(){
+	const date = new Date();
+	const minutes = date.getMinutes();
+	const hours = date.getHours();
+	const seconds = date.getSeconds();
+	clockTime.innerText = `${hours}:${minutes}:${seconds}`;
+	console.dir(date);
+}
+
+getTime();
+
+
