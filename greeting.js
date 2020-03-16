dea5ba5a3926ab1b6699aff89dd47b4de9cafdad
reumia19 <56555888+reumia19@users.noreply.github.com>
2020-03-16 16:17:54 +0900

    Add function loadname

---
+++ greeting.js
@@ -0,0 +1,12 @@
+const form = document.querySelector(".js-form");
+const input = document.querySelector("input");
+const greeting = document.querySelector(".js-greetings");
+
+
+function loadName(){
+	const currentUser = localStorage.getItem();
+}
+function init() {
+	loadName();
+}
+init():
