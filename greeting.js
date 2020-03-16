5470a2a8dcd7e37bc5497b48a389d071c44575bc
reumia19 <56555888+reumia19@users.noreply.github.com>
2020-03-16 22:02:38 +0900

    add greeting message

--- greeting.js
+++ greeting.js
@@ -2,11 +2,27 @@ const form = document.querySelector(".js-form");
 const input = document.querySelector("input");
 const greeting = document.querySelector(".js-greetings");
 
+const USER_LS = "currentUser";
+const SHOWING_CH = "showing";
+
+
+function saveName(text){
+	localStorage.setItem(USER_LS,text);
+}
+
+function handleSubmit(event){
+	const currentValue = input.value;
+	saveName(currentValue);
+}
+
+function painting
+
 
 function loadName(){
 	const currentUser = localStorage.getItem();
 }
+
 function init() {
 	loadName();
 }
-init():
+init();
