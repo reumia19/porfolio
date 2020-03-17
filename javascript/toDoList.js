58830230e93b920514e6885699fbd7ee51b13c61
reumia19 <56555888+reumia19@users.noreply.github.com>
2020-03-17 20:28:10 +0900

    add function

--- javascript/toDoList.js
+++ javascript/toDoList.js
@@ -2,5 +2,19 @@ const toDoForm = document.querySelector(".js-toDoForm");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.querySelector(".js-toDoList");
 
-const TODOS_LS ="toDoList";
+const TODOS_LS ="toDos";
 
+function init(){
+	loadToDoList();
+	toDoForm.addEventListener("submit",handleSubmit);
+}
+
+init();
+
+function loadToDoList(){
+
+}
+
+function handleSubmit(event){
+	
+}
