b45288058efefc5c265cfd536176532196ad1e6a
reumia19 <56555888+reumia19@users.noreply.github.com>
2020-03-17 20:24:22 +0900

    add const

--- javascript/toDoList.js
+++ javascript/toDoList.js
@@ -1,1 +1,6 @@
-const toDoList = document.querySelector(".js-toDolist");
+const toDoForm = document.querySelector(".js-toDoForm");
+const toDoInput = toDoForm.querySelector("input");
+const toDoList = document.querySelector(".js-toDoList");
+
+const TODOS_LS ="toDoList";
+
