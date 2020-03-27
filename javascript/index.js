b37ee48048b58c59055c46534419c5e42356d050
reumia19 <56555888+reumia19@users.noreply.github.com>
2020-03-27 22:42:34 +0900

    fix bug

--- javascript/index.js
+++ javascript/index.js
@@ -1,5 +1,5 @@
 // alert('아니 아직 아무것도 안 만들었는데... 벌써 올 줄은 몰랐지.. 나중에 오세요');
-const title =document.querySelector("#title");
+const title =document.querySelector(".title");
 const CLICKED_CLASS = "clicked";
 
 function handleClick(){
